c0a6aa8396f9ae58caa0bef489202ea89fa43948
manohar <mlselegant@gmail.com>
2026-03-08 16:19:42 +0545

    more dev

--- app/Filament/Resources/Categories/Pages/ListCategories.php
+++ app/Filament/Resources/Categories/Pages/ListCategories.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Categories\Pages;
 use App\Filament\Resources\Categories\CategoryResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCategories extends ListRecords
 {
@@ -15,5 +17,21 @@ class ListCategories extends ListRecords
         return [
             CreateAction::make()->modalWidth('md'),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'menu' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('show_in_menu', true)),
+            'others' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('show_in_menu', false)),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return 'menu';
     }
 }
