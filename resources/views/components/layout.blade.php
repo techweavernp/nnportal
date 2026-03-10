e214ef6645344c7dde000b338930c182e20d4d41
manohar <mlselegant@gmail.com>
2026-03-10 11:04:21 +0545

    fx

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,6 +23,14 @@
     <!-- Facebook OG Tags -->
     @endif
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M954ZNNS');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Google Fonts - Anek Devanagari -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -37,6 +45,10 @@
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M954ZNNS"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 {{$slot}}
 
