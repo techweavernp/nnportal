f761ce58f56c5e366d6f692d2b8ea284d08905c7
manohar <mlselegant@gmail.com>
2026-03-11 11:01:51 +0545

    fx

--- app/Filament/Resources/ManageAds/AdCampaigns/Tables/AdCampaignsTable.php
+++ app/Filament/Resources/ManageAds/AdCampaigns/Tables/AdCampaignsTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
@@ -21,6 +22,8 @@ class AdCampaignsTable
                     ->searchable(),
                 TextColumn::make('client.organization')
                     ->searchable(),
+                ImageColumn::make('image')
+                    ->openUrlInNewTab(),
                 TextColumn::make('start_date')
                     ->dateTime()
                     ->sortable(),
